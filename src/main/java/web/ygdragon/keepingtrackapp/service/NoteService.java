5274fed45a78459ee2cea841911b23bf33982ae2
ygdragon <djsdv@tut.by>
2024-02-02 02:40:49 +0300

    Complete task of Workshop6

---
+++ src/main/java/web/ygdragon/keepingtrackapp/service/NoteService.java
@@ -0,0 +1,82 @@
+package web.ygdragon.keepingtrackapp.service;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import web.ygdragon.keepingtrackapp.exception.NoteNotFoundException;
+import web.ygdragon.keepingtrackapp.model.Note;
+import web.ygdragon.keepingtrackapp.repository.NoteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    /**
+     * Finding one note by ID
+     *
+     * @param id ID finding note
+     * @return Findable note
+     */
+    public @NonNull Optional<Note> findNoteByID(Long id) {
+        return noteRepository.findById(id);
+    }
+
+    /**
+     * Find all notes
+     *
+     * @return List of available notes
+     */
+    public List<Note> findAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    /**
+     * Creating new note
+     *
+     * @param note Data for new note
+     * @return New note
+     */
+    public Note createNote(Note note) {
+        Note newNote = new Note();
+        newNote.setTitle(note.getTitle());
+        newNote.setContent(note.getContent());
+        return noteRepository.save(newNote);
+    }
+
+    /**
+     * Updating note
+     *
+     * @param note   Updatable note
+     * @param noteId ID of finding note
+     * @return Updated notes
+     */
+    public Note updateNote(Note note, Long noteId) {
+        Note uNote;
+        try {
+            uNote = noteRepository.findById(noteId).orElse(note);
+        } catch (NoteNotFoundException e) {
+            throw new NoteNotFoundException(noteId);
+        }
+        if (!uNote.equals(note)) {
+            uNote.setTitle(note.getTitle());
+            uNote.setContent(note.getContent());
+            return noteRepository.save(uNote);
+        } else {
+            System.out.println("Error>> ID not found");
+        }
+        return null;
+    }
+
+    /**
+     * Deleting note
+     *
+     * @param id ID of deleting note
+     */
+    public void deleteNote(Long id) {
+        noteRepository.deleteById(id);
+    }
+}
